ec4e29ac8908ae6661d55f88339c9038ac30ce6d
Jeff Long <banterability@users.noreply.github.com>
2016-10-05 10:31:56 -0500

    Whitelist file types to return

--- server/index.js
+++ server/index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const fs = require('fs');
 
 const imagePath = process.env.IMAGE_PATH;
+const imageTypes = ['gif', 'jpg', 'png', 'jpeg'];
 
 const app = express();
 
 app.get('/', (req, res) => {
   fs.readdir(imagePath, (err, files) => {
     console.log('err', err);
-    res.send(files);
+
+    const results = files.reduce((memo, filename) => {
+      imageTypes.includes(filename.split('.')[1])
+        ? memo.shown.push(filename)
+        : memo.hidden.push(filename);
+
+      return memo;
+    }, {
+      hidden: [],
+      shown: []
+    });
+
+    console.log(`Hiding ${results.hidden.length} non-image files`);
+    res.send(results.shown);
   })
 });
 
